029f4739a38bbd0ccdbab2cc8fe76ac168f0988e
ArtemRazmeritsa <artemrazmeritsa@yandex.ru>
2025-01-28 19:46:16 +0300

    correct comments

---
+++ src/components/app/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState, CSSProperties } from 'react';
+import { Article } from '../article/Article';
+import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
+import 'src/styles/index.scss';
+import styles from 'src/styles/index.module.scss';
+
+export const App = () => {
+	const [articleState, setArticleState] = useState(defaultArticleState);
+
+	const handleArticleStateChange = (newState: ArticleStateType) => {
+		setArticleState((prevState) => ({ ...prevState, ...newState }));
+	};
+
+	return (
+		<main
+			className={styles.main}
+			style={
+				{
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm onChange={handleArticleStateChange} />
+			<Article />
+		</main>
+	);
+};
